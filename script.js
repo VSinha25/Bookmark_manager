15bb1f289ce2f87783117a81cffa66af32019d80
Vignesh Sinha <vigneshsinha25@gmail.com>
2024-08-13 22:29:14 +0530

    finished

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const modalClose = document.getElementById('close-modal');
 const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
-const bookmarkContainer = document.getElementById('bookmarks-container');
+const bookmarksContainer = document.getElementById('bookmarks-container');
 
 
-    let bookmarks = [];
+    let bookmarks = {};
 
 //Show Modal, Focus on Input
 
@@ -46,9 +46,35 @@ const bookmarkContainer = document.getElementById('bookmarks-container');
 // Build Bookmarks DOM
 
     function buildBookmarks() {
+        // Remove all bookmark elements
+        bookmarksContainer.textContent = ''; 
         // Build Items 
         bookmarks.forEach((bookmark) => {
             const { name, url } = bookmark;
+            // Item
+            const item = document.createElement('div');
+            item.classList.add('item');
+            // Close icon
+            const closeIcon = document.createElement('i');
+            closeIcon.classList.add('fa-solid','fa-xmark');
+            closeIcon.setAttribute('title', 'Delete Bookmark');
+            closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+            // Favicon / Link Constainer
+            const linkInfo = document.createElement('div');
+            linkInfo.classList.add('name');
+            // Favicon
+            const favicon = document.createElement('img');
+            favicon.setAttribute('src', `https://www.google.com/s2/favicons?domain=${url}`);
+            favicon.setAttribute('alt', 'Favicon');
+            //Link
+            const link = document.createElement('a');
+            link.setAttribute('href', `${url}`);
+            link.setAttribute('target', '_blank');
+            link.textContent = name;
+            // Append to bookmarks container
+            linkInfo.append(favicon, link);
+            item.append(closeIcon, linkInfo);
+            bookmarksContainer.appendChild(item);
         });
     }
 
@@ -70,6 +96,18 @@ const bookmarkContainer = document.getElementById('bookmarks-container');
             localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
         }
         buildBookmarks();
+    }
+
+// Delete Bookmarks
+    function deleteBookmark(url){
+        bookmarks.forEach((bookmark, i) => {
+            if(bookmark.url === url){
+                bookmarks.splice(i,1);
+            }    
+        });
+        // Update bookmarks array in localStorage, re-populate DOM
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        fetchBookmarks();
     }
 
 // Handle data from Form
@@ -99,7 +137,7 @@ const bookmarkContainer = document.getElementById('bookmarks-container');
     
 
 //Event Listener
-    bookmarkForm.addEventListener('submit', storeBookmark);
+        bookmarkForm.addEventListener('submit', storeBookmark);
 
 // On Load, Fetch Bookmarks
-    fetchBookmarks();
+        fetchBookmarks();
